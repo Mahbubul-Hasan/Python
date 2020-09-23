7a926628a38110d98129865c65cd7ad873a93afb
Mahbubul Hasan <mahbubul161@gmail.com>
2020-09-24 00:45:31 +0600

    print()

---
+++ print/index.py
@@ -0,0 +1,25 @@
+print("Hello Python")
+
+print(25)
+
+print(30 * 10)
+
+print("50 + 10 = ", 50 + 10)
+
+print("Hello", "", "Word")
+
+print("{0} - {1} = {2}".format(50, 10, 50 - 10))
+
+print("Hello", "Word", sep="---")
+
+name = "Max"
+print("Name = %s" % name)
+
+age = 24
+print("Hello %s, Are you %d?" % (name, age))
+
+print("integerNumber = %d" % 161)
+
+print("floatNumber = %f" % 161.15)
+
+print("floatNumber = %.2f" % 161.15)

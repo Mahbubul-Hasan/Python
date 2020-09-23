bbddadf89e64f773d049dfd0a53a690d1edba755
Mahbubul Hasan <mahbubul161@gmail.com>
2020-09-24 00:58:43 +0600

    input()

---
+++ input/index.py
@@ -0,0 +1,11 @@
+# number = input("Enter a number: ")
+
+# print(number)
+
+# floatNumber = input("Enter a float number: ")
+
+# print(floatNumber)
+
+floatNumber = int(input("Enter a float number: "))
+
+print(floatNumber)

6cf083bab1f9f223728e2c2a79bd89bfd6cba5e8
Mahbubul Hasan <mahbubul161@gmail.com>
2020-09-25 09:30:22 +0600

    math-operators

---
+++ math-operators/index.py
@@ -0,0 +1,24 @@
+print(50 + 10)
+
+print(60 - 50)
+
+print(- 60 + 50)
+
+print(50 / 3)
+
+print(50 // 3)
+
+print(50.0 / 3.0)
+
+print(50 * 10)
+
+print(10 % 3)
+
+print(2 ** 3)
+
+print(2e3)
+print(2E3)
+
+print(5 + 9 - 6 * 10)
+
+print(5 + 9 - 6 * 10 / 20)

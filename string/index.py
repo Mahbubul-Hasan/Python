d325b5d7b5fabfc3b74973dfebebaf8f7ce1b6e4
Mahbubul Hasan <mahbubul161@gmail.com>
2020-09-25 10:49:52 +0600

    string functions

---
+++ string/index.py
@@ -0,0 +1,22 @@
+x = "Hello Word"
+y = "hello word"
+z = "  hello word   "
+
+print(x)
+print(y)
+
+print(x[0])
+print(y[1:4])
+
+print(z.strip())
+
+print(x.lower())
+print(y.upper())
+print(y.capitalize())
+
+print(x.isupper())
+print(y.islower())
+
+print(x.replace("H", "J"))
+
+print("Hello " * 10)

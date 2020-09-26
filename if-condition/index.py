c5f180c2f3b1c14a3d16f43cff7e70ab0c23e2f6
Mahbubul Hasan <mahbubul161@gmail.com>
2020-09-26 09:29:13 +0600

    If & if-else conditions

---
+++ if-condition/index.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+if number != 100 and number > 0:
+    print("Number = %d" % number)
+
+if number > 0:
+    print(number, "is peositive")
+
+else:
+    print(number, "is negative")
---
+++ if-else-condition/index.py
@@ -0,0 +1,16 @@
+name = input("Enter your name: ")
+
+if name == "Max":
+    print("Name = ", name)
+
+elif name == "Min":
+    print("Name = ", name)
+
+elif name == "Roy":
+    print("Name = ", name)
+
+elif name == "Eli":
+    print("Name = ", name)
+
+else:
+    print("The name entered is invalid ")

8e9e1271506bd5822b4b118c63721773f674a5aa
Mahbubul-Hasan <mahbubul161@gmail.com>
2020-09-26 11:27:00 +0600

    Learn: set

--- set/index.py
+++ set/index.py
@@ -1,34 +1,34 @@
 A = {3, 5, 6, 7, 2, 4, 4, 5, 3, 5, 8, 8, 0}
 B = set((2, 5, 7, 9, 1))
 
-# print(A)
+print(A)
 
-# print(len(A))
+print(len(A))
 
-# A.add(10)
-# A.update([15, 34, 11, 20])
-# A.remove(8)
-# A.discard(8)
-# print(A)
+A.add(10)
+A.update([15, 34, 11, 20])
+A.remove(8)
+A.discard(8)
+print(A)
 
-# Name = {"Max", "Tom", "Reek"}
+Name = {"Max", "Tom", "Reek"}
 
-# Name.pop()
+Name.pop()
 
-# Name.clear()
+Name.clear()
 
-# del Name
+del Name
 
-# print(Name)
+print(Name)
 
-# print(A | B)
-# print(A.union(B))
+print(A | B)
+print(A.union(B))
 
-# print(A & B)
-# print(A.intersection(B))
+print(A & B)
+print(A.intersection(B))
 
-# print(A ^ B)
-# print(A.symmetric_difference(B))
+print(A ^ B)
+print(A.symmetric_difference(B))
 
-# print(A - B)
-# print(A.difference(B))
+print(A - B)
+print(A.difference(B))

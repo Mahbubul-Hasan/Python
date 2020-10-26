7534c7556526d860ad7caa0deaf568cb826c4f51
Mahbubul-Hasan <mahbubul161@gmail.com>
2020-10-26 11:19:07 +0600

    Learn:  String Formatting

--- print/index.py
+++ print/index.py
@@ -10,6 +10,12 @@ print("Hello", "", "Word")
 
 print("{0} - {1} = {2}".format(50, 10, 50 - 10))
 
+num1 = 3.14387548253492
+num2 = 73.852735912372
+print('{0:.3f} + {1:.3f} = {2:.3f}'.format(num1, num2, (num1 + num2)))
+
+print(f'{num1:.3f} + {num2:.3f} = {(num2 + num2):.3f}')
+
 print("Hello", "Word", sep="---")
 
 name = "Max"

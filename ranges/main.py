645e70a2fb674e2d5d073dd445fa8894ac5aa4a7
Mahbubul-Hasan <mahbubul161@gmail.com>
2020-10-26 13:27:58 +0600

    Learn: Range

---
+++ ranges/main.py
@@ -0,0 +1,17 @@
+# for i in range(5):
+#     print(i)
+
+
+# for i in range(3, 11):
+#     print(i)
+
+# for i in range(0, 21, 4):
+#     print(i)
+
+burgers = ['beef', 'chicken', 'veg', 'supreme', 'doub']
+
+# for i in range(len(burgers)):
+#     print(f'{i} => {burgers[i]}')
+
+for i in range(len(burgers)-1, -1, -1):
+    print(f'{i} => {burgers[i]}')
